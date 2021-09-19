20ea53e6395185f3a0e43c08dec03e73243bdf1d
ff032167 <dmi32167@yandex.ru>
2021-09-19 20:18:55 +0300

    исправил название переменной

--- internal/currency.go
+++ internal/currency.go
@@ -25,23 +25,23 @@ type Rate struct {
 }
 
 func Relation(ctx context.Context, storage Storage, date string, curr1, curr2 string) (string, error) {
-	data, err := storage.Rate(ctx, date)
+	rates, err := storage.Rate(ctx, date)
 	if err != nil {
 		return "", fmt.Errorf("cant get rate from storage: %w", err)
 	}
-	curr1index := sort.Search(len(data), func(i int) bool {
-		return data[i].CurrCode >= curr1
+	curr1index := sort.Search(len(rates), func(i int) bool {
+		return rates[i].CurrCode >= curr1
 	})
 
-	curr2index := sort.Search(len(data), func(i int) bool {
-		return data[i].CurrCode >= curr2
+	curr2index := sort.Search(len(rates), func(i int) bool {
+		return rates[i].CurrCode >= curr2
 	})
 
-	if data[curr2index].Rate == float64(0) {
+	if rates[curr2index].Rate == float64(0) {
 		return "", errors.New("error, rate of the " + curr2 + " is 0")
 	}
 
-	relation := data[curr2index].Rate / data[curr1index].Rate
+	relation := rates[curr2index].Rate / rates[curr1index].Rate
 
 	return "exchange rate for " + curr1 + " to " + curr2 + " is " +
 		strconv.FormatFloat(relation, 'f', 6, 64), nil
